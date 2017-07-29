34380b522142ec2bb3b0ce99a15d827307e5a1a9
Stewart MacKenzie-Leigh <stewartml@gmail.com>
2017-07-29 21:18:21 +0100

    wip

--- src/test/index.tsx
+++ src/test/index.tsx
@@ -1,113 +1,31 @@
 import test from 'ava';
-import { contextProvider, contextToProps } from '../lib';
+import { contextProvider, contextConsumer } from '../lib';
 import { shallow, mount } from 'enzyme';
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
 require('jsdom-global')();
 
-interface ContextTypes {
+interface TestContext {
   foo: string;
 }
 
-const contextTypes = {
-  foo: PropTypes.string
-}
+test('contextProvider / contextConsumer', (t) => {
+  const Provider = contextProvider<TestContext>('test');
 
-test('contextProvider', (t) => {
-  t.plan(2);
+  const Consumer = contextConsumer<TestContext>('test')(
+    (props) => <span>{props.foo}</span>
+  );
 
-  const Provider = contextProvider<ContextTypes, {baz: string}>(contextTypes)((props) => {
-    t.deepEqual(props.baz, 'bang');
-    return <div>{props.children}</div>;
-  });
-
-  const Consumer: React.StatelessComponent<any> = (props, context) => {
-    t.deepEqual(context, {foo: 'bar'});
-    return <span />;
-  };
-
-  Consumer.contextTypes = contextTypes;
-
-  mount(
-    <Provider foo='bar' baz='bang'>
+  const wrapper = mount(
+    <Provider foo='hello'>
       <Consumer />
     </Provider>
   );
-});
 
+  const span = wrapper.find('span');
+  t.is(span.text(), 'hello');
 
-test('contextProvider with pass-through', (t) => {
-  t.plan(2);
-
-  const Provider = contextProvider<ContextTypes>(contextTypes, true)((props) => {
-    t.deepEqual(props.foo, 'bar');
-    return <div>{props.children}</div>;
-  });
-
-  const Consumer: React.StatelessComponent<any> = (props, context) => {
-    t.deepEqual(context, {foo: 'bar'});
-    return <span />;
-  };
-
-  Consumer.contextTypes = contextTypes;
-
-  mount(
-    <Provider foo='bar'>
-      <Consumer />
-    </Provider>
-  );
-});
-
-
-test('contextProvider with pass-through array', (t) => {
-  t.plan(2);
-
-  const Provider = contextProvider<ContextTypes>(contextTypes, ['foo'])((props) => {
-    t.deepEqual(props.foo, 'bar');
-    return <div>{props.children}</div>;
-  });
-
-  const Consumer: React.StatelessComponent<any> = (props, context) => {
-    t.deepEqual(context, {foo: 'bar'});
-    return <span />;
-  };
-
-  Consumer.contextTypes = contextTypes;
-
-  mount(
-    <Provider foo='bar'>
-      <Consumer />
-    </Provider>
-  );
-});
-
-
-test('contextToProps', (t) => {
-  t.plan(1);
-
-  class Provider extends React.Component<any, any> {
-    static childContextTypes = contextTypes;
-
-    getChildContext() {
-      return {
-        foo: 'bar'
-      };
-    }
-
-    render() {
-      return <div>{this.props.children}</div>;
-    }
-  }
-
-  const Consumer = contextToProps<ContextTypes>(contextTypes)((props) => {
-    t.deepEqual(props, {foo: 'bar'});
-    return <span/>;
-  });
-
-  mount(
-    <Provider foo='bar'>
-      <Consumer />
-    </Provider>
-  );
+  wrapper.setProps({foo: 'bye'});
+  t.is(span.text(), 'bye');
 });
